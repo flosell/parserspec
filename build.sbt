1cd1cc1bbdd96f85004df779225a18862353a970
Florian Sellmayr <sellmayr@in.tum.de>
2013-06-27 17:04:51 +1000

    initial checkin. first working version of ParserSpec

---
+++ build.sbt
@@ -0,0 +1,13 @@
+name := "ParserSpec"
+
+organization := "org.parserspec"
+
+version := "0.0.1-SNAPSHOT"
+
+scalaVersion := "2.10.1"
+
+libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.0.M5b" 
+
+scalacOptions ++= Seq("-feature")
+
+
---
+++ src/main/scala/org/parserspec/ParserSpec.scala
@@ -0,0 +1,46 @@
+package org.parserspec
+import org.scalatest.matchers.ShouldMatchers
+import scala.util.parsing.input.CharSequenceReader
+import org.scalatest.FunSpec
+import scala.util.parsing.combinator.RegexParsers
+import scala.language.implicitConversions
+/**
+ * Usage: 
+ * <pre>
+ * class SomeParserSpec extends AbstractParserTest with SomeParserClass{
+ * 	
+ * 	describe("the parser") {
+ * 	  someParser mustParse "access" to ACCESS
+ * 	  someParser mustParse "call" to CALL
+ * 	}
+ * }
+</pre>
+ */
+abstract class ParserSpec extends FunSpec with ShouldMatchers with RegexParsers{
+  private def parsing[T](s: String)(implicit p: Parser[T]): T = {
+    // thanks to http://henkelmann.eu/2011/01/29/an_introduction_to_scala_parser_combinators-part_3_unit_tests
+    //wrap the parser in the phrase parse to make sure all input is consumed
+    val phraseParser = phrase(p)
+    //we need to wrap the string in a reader so our parser can digest it
+    val input = new CharSequenceReader(s)
+    phraseParser(input) match {
+      case Success(t, _) => t
+      case NoSuccess(msg, _) => throw new IllegalArgumentException(
+        "Could not parse '" + s + "': " + msg)
+    }
+  }
+
+  case class ParserWord[T](parser: Parser[T]) {
+    def mustParse(s: String) = MustParsePart(parser, s)
+  }
+
+  case class MustParsePart[T](parser: Parser[T], s: String) {
+    def to(expectedResult: T) = it("must parse '" + s + "' to " + expectedResult) {
+      parsing(s)(parser) should equal(expectedResult)
+    }
+    
+    def toAnything = it("must succeed parsing parsing'"+s+"'") {parsing(s)(parser)}
+  }
+
+  implicit def toParserWord[T](parser: Parser[T]) = ParserWord(parser)
+}
